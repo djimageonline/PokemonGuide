91afd74010b0e3b27d85843f69033b55dfdac2e9
Johnny Proano <jproano22@yahoo.com>
2024-09-24 08:33:19 -0400

    Initial Commit

---
+++ PokemonGuide/PokemonGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonGuideApp.swift
+//  PokemonGuide
+//
+//  Created by Johnny Proano on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct PokemonGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
